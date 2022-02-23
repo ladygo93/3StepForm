c14c4339f170b4e9ee879f7453b29ad62d0dec5d
ladygo93 <65852371+ladygo93@users.noreply.github.com>
2022-02-23 01:43:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.css'
 import Form from './Components/Form/Form'
-import { FormDataProvider } from './context/FormContext'
 
 function App() {
   return (
     <div className='container'>
-      <FormDataProvider>
-        <Form />
-      </FormDataProvider>
+      <Form />
     </div>
   )
 }
